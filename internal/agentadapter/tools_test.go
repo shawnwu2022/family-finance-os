ba59c9615fad38e4a6350f4bd3ff3aa8624a6f16
shanwn wu <111497504+shawnwu2022@users.noreply.github.com>
2026-08-18 21:11:17 +0800

    test(agentadapter): expect spending analysis tool

--- internal/agentadapter/tools_test.go
+++ internal/agentadapter/tools_test.go
@@ -18,6 +18,7 @@ func TestDefinitionsExposeOnlyImplementedReadAndSimulationCapabilities(t *testin
 		ToolGetGoalStatus,
 		ToolGetHouseholdOverview,
 		ToolGetSafeToSpend,
+		ToolGetSpendingAnalysis,
 		ToolSimulateExtraDebtPayment,
 		ToolSimulateGoal,
 		ToolSimulatePurchase,
